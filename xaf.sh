e63819e346132d6394f76955c7b618ce5d87393f
alonewolfpro <129979792+alonewolfpro@users.noreply.github.com>
2023-04-06 15:57:55 +0300

    Update xaf.sh

--- xaf.sh
+++ xaf.sh
@@ -2,9 +2,9 @@ apt update
 apt-get install python3 git nmap dnsenum nikto python3-pip python2 ruby php  perl nano vim  openssl nodejs tor
 pip3 install colorama requests 
 git clone https://github.com/ruped24/toriptables3
-git clone https://github.com/ruped24/tor_ip_switcher
+git clone https://github.com/1N3/Sn1per
 cd toriptables3
 python3 toriptables3.py -l
 cd ..
-cd tor_ip_switcher 
-python3 tor_ip_switcher.py 
+cd Sn1per
+sh install.sh
